9817b6bec8bc419a49ff33de454be862edbd1ce9
Đào Quang Hưng <71087377+hung-cell@users.noreply.github.com>
2022-09-12 00:14:11 +0700

    Backend-Base-Project-Spring-boot: Add BussinessExceptionHandler

--- src/main/java/source/exception/BusinessException.java
+++ src/main/java/source/exception/BusinessException.java
@@ -1,6 +1,7 @@
 package source.exception;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 import java.io.Serializable;
 
@@ -14,6 +15,10 @@ public class BusinessException extends RuntimeException implements Serializable 
     private static final long serialVersionUID = 1905122041950251207L;
     private final BusinessError error;
 
+    public BusinessException(int code, String message, HttpStatus status) {
+       super(message);
+       this.error = new BusinessError(code, message, status);
+    }
     public BusinessException(BusinessError error) {
         super(error.getMessage());
         this.error = error;

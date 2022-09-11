e29af3291a3eb3769005d62c0163c5f2abc17da4
Đỗ Hùng Anh <anh.do10@onemount.com>
2022-09-11 23:55:55 +0700

    Backend-Project-Spring-boot-Example: Update project example

--- src/main/java/com/onemount/infrastructure/repo/JpaBookingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.onemount.infrastructure.repo;
-
-import com.onemount.infrastructure.repo.model.JpaBooking;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JpaBookingRepository extends JpaRepository<JpaBooking, Long> {
-}
--- src/main/resources/db/migration/V1.0__init.sql
+++
@@ -1,7 +0,0 @@
-create table bookings
-(
-    booking_id  bigint      not null,
-    create_time datetime(6) not null,
-    update_time datetime(6) not null,
-    primary key (booking_id)
-);

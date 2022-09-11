4de220538242f7fd69d7b745b7e2d49482213914
Đỗ Hùng Anh <anh.do10@onemount.com>
2022-09-11 12:10:25 +0700

    Backend-Base-Project-Spring-boot: Add package third_party

--- src/main/java/source/exception/BusinessErrors.java
+++ src/main/java/source/exception/BusinessErrors.java
@@ -11,17 +11,17 @@ public class BusinessErrors {
     /**
      * 400
      */
-    public static final BusinessError INVALID_PARAMETERS = new BusinessError(400, "Invalid parameters", HttpStatus.BAD_REQUEST);
+    public static final BusinessError INVALID_PARAMETERS = new BusinessError(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.getReasonPhrase(), HttpStatus.BAD_REQUEST);
 
     /**
      * 403
      */
-    public static final BusinessError FORBIDDEN_ERROR = new BusinessError(403, "You don not have any permissions to access this resource", HttpStatus.FORBIDDEN);
+    public static final BusinessError FORBIDDEN_ERROR = new BusinessError(HttpStatus.FORBIDDEN.value(), HttpStatus.FORBIDDEN.getReasonPhrase(), HttpStatus.FORBIDDEN);
 
     /**
      * 500
      */
-    public static final BusinessError INTERNAL_SERVER_ERROR = new BusinessError(500, "Internal server error", HttpStatus.INTERNAL_SERVER_ERROR);
+    public static final BusinessError INTERNAL_SERVER_ERROR = new BusinessError(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(), HttpStatus.INTERNAL_SERVER_ERROR);
 
     private BusinessErrors() {
     }

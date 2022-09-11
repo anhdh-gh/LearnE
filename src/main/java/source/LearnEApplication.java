1966dade8cf1d608506e1d6786428e184a732bfa
Đào Quang Hưng <71087377+hung-cell@users.noreply.github.com>
2022-09-11 22:07:22 +0700

    Backend-Service-Gateway: add api sign in and sign up

--- src/main/java/source/LearnEApplication.java
+++ src/main/java/source/LearnEApplication.java
@@ -2,12 +2,21 @@ package source;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@PropertySource(value = "classpath:error.properties", encoding = "UTF-8")
 public class LearnEApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LearnEApplication.class, args);
+    }
+
+    @Bean
+    RestTemplate getRestTemplate(){
+        return new RestTemplate();
     }
 
 }
---
+++ src/main/java/source/dto/request/UserSignUpRequestDto.java
@@ -0,0 +1,34 @@
+package source.dto.request;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class UserSignUpRequestDto extends BasicRequest{
+
+
+    @NotNull(message = "400001")
+    @Email(message = "400002")
+    @Size(min = 6, max = 100, message = "400003")
+    private String email;
+
+    @NotNull(message = "400004")
+    @Size(min = 6, max = 500, message = "400005")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$", message = "400006")
+    private String password;
+
+    @JsonProperty("username")
+    @NotNull(message = "400007")
+    @Size(max = 500, message = "400008")
+    private String userName;
+}

c2938aaee85c7928aef482e3d4cf1b69b27a30c8
Đỗ Hùng Anh <anh.do10@onemount.com>
2022-09-11 12:10:25 +0700

    Backend-Base-Project-Spring-boot: Add package third_party

--- src/main/java/source/dto/request/BasicRequest.java
+++ src/main/java/source/dto/request/BasicRequest.java
@@ -1,7 +1,5 @@
 package source.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;

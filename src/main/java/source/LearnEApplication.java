fe335e4c793c55d329ef7ebce1813e3545aa84b3
Đỗ Hùng Anh <anh.do10@onemount.com>
2022-09-12 23:37:59 +0700

    Backend-Service-User: Add read error.properties

--- src/main/java/source/LearnEApplication.java
+++ src/main/java/source/LearnEApplication.java
@@ -3,9 +3,11 @@ package source;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+@PropertySource(value = "classpath:error.properties", encoding = "UTF-8")
 public class LearnEApplication {
 
     public static void main(String[] args) {

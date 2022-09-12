9f1e955f62719f1f459691af6c18d099b551ac72
Đào Quang Hưng <71087377+hung-cell@users.noreply.github.com>
2022-09-12 10:53:47 +0700

    Backend-Base-Project-Spring-boot: add @Data, @NoArgConstructor, @AllArgsConstructor
    This reverts commit a430d3669939b6f6d47327c80e77261f9164f362.

--- src/main/java/source/exception/BusinessError.java
+++ src/main/java/source/exception/BusinessError.java
@@ -1,7 +1,9 @@
 package source.exception;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 import java.io.Serializable;
@@ -9,8 +11,9 @@ import java.io.Serializable;
 /**
  * Business Error Data Structure
  */
-@Getter
+@Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class BusinessError implements Serializable {
     private static final long serialVersionUID = 2405172041950251807L;
     private int code;

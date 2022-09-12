13834e31d351251ac8ac4c9830561d015697a941
Đỗ Hùng Anh <anh.do10@onemount.com>
2022-09-12 23:17:03 +0700

    Backend-Service-User: Pussh ErrorCodeConstant

---
+++ src/main/java/source/constant/ErrorCodeConstant.java
@@ -0,0 +1,17 @@
+package source.constant;
+
+public class ErrorCodeConstant {
+
+    private ErrorCodeConstant() {}
+
+    public static final String EMAIL_IS_NOT_NULL_400001 = "400001";
+    public static final String EMAIL_IS_NOT_VALID_400002 = "400002";
+    public static final String EMAIL_MUST_HAVE_AT_LEAST_6_CHARACTERS_AND_NO_MORE_THAN_100_CHARACTERS_400003 = "400003";
+    public static final String PASSWORD_IS_NOT_NULL_400004 = "400004";
+    public static final String PASSWORD_MUST_NO_MORE_THAN_100_CHARACTERS_400005 = "400005";
+    public static final String PASSWORD_IS_NOT_STRONG_400006 = "400006";
+    public static final String USERNAME_IS_NOT_NULL_400007 = "400007";
+    public static final String USERNAME_MUST_NO_MORE_THAN_100_CHARACTERS_400008 = "400008";
+    public static final String EMAIL_ALREADY_EXISTS_400009 = "400009";
+    public static final String USERNAME_ALREADY_EXISTS_400010 = "400010";
+}

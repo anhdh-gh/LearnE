1f9f6c21c796e1a95192717b34c5407373bca074
Đỗ Hùng Anh <anh.do10@onemount.com>
2022-09-12 23:41:07 +0700

    Backend-Base-Project-Spring-boot: Add read error.properties

--- src/main/java/source/LearnEApplication.java
+++ src/main/java/source/LearnEApplication.java
@@ -2,8 +2,10 @@ package source;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = "classpath:error.properties", encoding = "UTF-8")
 public class LearnEApplication {
 
     public static void main(String[] args) {

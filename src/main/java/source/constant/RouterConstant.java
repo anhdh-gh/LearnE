d40a5b83f331b83ed3af8396a360618922d7b366
Đỗ Hùng Anh <anh.do10@onemount.com>
2022-09-13 16:05:03 +0700

    Backend-Service-Gateway: Update RouterConstant

--- src/main/java/source/constant/RouterConstant.java
+++ src/main/java/source/constant/RouterConstant.java
@@ -6,10 +6,5 @@ public class RouterConstant {
 
     public final static String SIGN_UP = "/user/signup";
 
-    public final static String GET_ALL_USERS = "/user";
-
-    public final static String GET_USER_BY_ID = "/user/{id}";
-
-
     private RouterConstant() {}
 }

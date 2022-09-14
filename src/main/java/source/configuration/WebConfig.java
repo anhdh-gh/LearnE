f980e0ffd74daa7c6d92c17409a781d31b273c7e
Đỗ Hùng Anh <anh.do10@onemount.com>
2022-09-14 18:12:26 +0700

    Backend-Service-Auth: Update WebConfig

--- src/main/java/source/configuration/WebConfig.java
+++ src/main/java/source/configuration/WebConfig.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
 
     @Value("${service.gateway.baseurl}")
     private String allowedOrigin;

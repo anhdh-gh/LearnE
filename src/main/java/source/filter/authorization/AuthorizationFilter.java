57ea20b000e19238c2281c4df0cfe49e8bbfec0d
Đỗ Hùng Anh <anh.do10@onemount.com>
2022-09-14 17:08:49 +0700

    Backend-Service-Auth: Update AuthorizationFilter

--- src/main/java/source/filter/authorization/AuthorizationFilter.java
+++ src/main/java/source/filter/authorization/AuthorizationFilter.java
@@ -28,7 +28,7 @@ public class AuthorizationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         try {
             User user = (User) request.getAttribute(RequestKeyConstant.USER);
-            if(user != null && user.getRole().equals(Role.USER)) {
+            if(user != null && user.getRole().equals(Role.ADMIN)) {
                 filterChain.doFilter(request, response);
             } else {
                 handlerError(request, response);

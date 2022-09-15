edd3e3f468d1741962a3b943217e23c2df1dcf6d
Đỗ Hùng Anh <anh.do10@onemount.com>
2022-09-15 21:34:18 +0700

    Backend-Service-Auth: Refactor code

--- src/main/java/source/filter/authentication/AuthenticationConfiguration.java
+++ src/main/java/source/filter/authentication/AuthenticationConfiguration.java
@@ -12,11 +12,12 @@ public class AuthenticationConfiguration {
     public FilterRegistrationBean<AuthenticationFilter> authenticationFilter() {
         FilterRegistrationBean<AuthenticationFilter> registrationBean = new FilterRegistrationBean<>();
         registrationBean.setFilter(new AuthenticationFilter());
-        registrationBean.addUrlPatterns(RouterConstant.USER_GET_ALL,
-                                            RouterConstant.USER_DELETE,
-                                            RouterConstant.USER_UPDATE,
-                                            RouterConstant.USER_GET_INFO,
-                                            RouterConstant.USER_GET_INFO);
+        registrationBean.addUrlPatterns(
+            RouterConstant.USER_GET_ALL,
+            RouterConstant.USER_DELETE,
+            RouterConstant.USER_UPDATE,
+            RouterConstant.USER_GET_INFO
+        );
         registrationBean.setOrder(1);
         return registrationBean;
     }

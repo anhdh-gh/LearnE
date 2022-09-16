98825878eccd942397aa6fe86db2d5a18d6abc74
Đào Quang Hưng <71087377+hung-cell@users.noreply.github.com>
2022-09-17 05:09:25 +0700

    Backend-Service-Gateway: Add validate page and size

--- src/main/java/source/dto/request/UserGetAllRequestDto.java
+++ src/main/java/source/dto/request/UserGetAllRequestDto.java
@@ -3,13 +3,19 @@ package source.dto.request;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import source.constant.ErrorCodeConstant;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserGetAllRequestDto extends BasicRequest{
 
+    @Min(value = 0, message = ErrorCodeConstant.PAGE_IS_GREATER_THAN_MINUS_ONE_400029)
     private int page;
 
+    @Min(value =0, message = ErrorCodeConstant.SIZE_IS_GREATER_THAN_MINUS_ONE_400030)
     private int size;
 }

9250cda52955273039df358e8c5b6552d26b7798
Đỗ Hùng Anh <anh.do10@onemount.com>
2022-09-20 23:58:34 +0700

    Backend-Service-Multimedia: Upload CorsFilter

---
+++ src/main/java/source/constant/RequestKeyConstant.java
@@ -0,0 +1,14 @@
+package source.constant;
+
+public class RequestKeyConstant {
+
+    private RequestKeyConstant() {}
+
+    public static final String REQUEST_ID = "request_id";
+
+    public static final String URI = "uri";
+
+    public static final String X_REQUEST_ID = "X-Request-ID";
+
+    public static final String CONTENT_TYPE = "Content-type";
+}
--- src/main/java/source/filter/CorsFilter.java
+++ src/main/java/source/filter/CorsFilter.java
@@ -1,15 +1,15 @@
 package source.filter;
 
 import org.json.simple.JSONObject;
-import org.json.simple.parser.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ObjectUtils;
-import org.springframework.util.StringUtils;
 import org.json.simple.parser.JSONParser;
+import source.constant.ContentTypeConstant;
+import source.constant.RequestKeyConstant;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
@@ -21,57 +21,63 @@ import java.util.UUID;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
 
-  /** The logger. */
-  private Logger logger = LoggerFactory.getLogger(CorsFilter.class);
+    /** The logger. */
+    private Logger logger = LoggerFactory.getLogger(CorsFilter.class);
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
-   */
-  @Override
-  public void init(FilterConfig filterConfig) throws ServletException {}
+    /*
+     * (non-Javadoc)
+     *
+     * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
+     */
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest, javax.servlet.ServletResponse,
-   * javax.servlet.FilterChain)
-   */
-  @SuppressWarnings("unchecked")
-  @Override
-  public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-    HttpServletResponse response = (HttpServletResponse) res;
-    HttpServletRequest request = (HttpServletRequest) req;
+    /*
+     * (non-Javadoc)
+     *
+     * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest, javax.servlet.ServletResponse,
+     * javax.servlet.FilterChain)
+     */
+    @SuppressWarnings("unchecked")
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
 
-    try {
-      ApiKeyVerifyRequestWrapper requestWrapper = new ApiKeyVerifyRequestWrapper(request);
+        String contentType = request.getHeader(RequestKeyConstant.CONTENT_TYPE);
+        String requestId = request.getHeader(RequestKeyConstant.X_REQUEST_ID);
+        if (requestId == null || requestId.isEmpty()) {
+            requestId = UUID.randomUUID().toString();
+        }
+        request.setAttribute(RequestKeyConstant.REQUEST_ID, requestId);
 
-      JSONParser parser = new JSONParser();
-      JSONObject dataRequest = ObjectUtils.isEmpty(requestWrapper.getBody()) ? new JSONObject()
-        : (JSONObject) parser.parse(requestWrapper.getBody());
-      String requestId = requestWrapper.getHeader("X-Request-ID");
-      if (requestId == null || requestId.isEmpty()) {
-        requestId = UUID.randomUUID().toString();
-      }
-      dataRequest.put("request_id", requestId);
-      request.setAttribute("request_id", requestId);
-      dataRequest.put("uri", request.getRequestURI());
-      dataRequest.put("authorization", requestWrapper.getHeader("Authorization"));
-      requestWrapper.setBody(dataRequest.toString());
-      chain.doFilter(requestWrapper, res);
-    } catch (Exception e) {
-      logger.error(e.toString(), e);
-      response.setStatus(HttpServletResponse.SC_BAD_GATEWAY);
+        if(contentType != null && contentType.trim().toLowerCase().contains(ContentTypeConstant.MULTIPART_FORM_DATA)) {
+            chain.doFilter(request, response);
+        } else {
+            try {
+                ApiKeyVerifyRequestWrapper requestWrapper = new ApiKeyVerifyRequestWrapper(request);
+
+                JSONParser parser = new JSONParser();
+                JSONObject dataRequest = ObjectUtils.isEmpty(requestWrapper.getBody())
+                    ? new JSONObject()
+                    : (JSONObject) parser.parse(requestWrapper.getBody());
+
+                dataRequest.put(RequestKeyConstant.REQUEST_ID, requestId);
+                dataRequest.put(RequestKeyConstant.URI, request.getRequestURI());
+                requestWrapper.setBody(dataRequest.toString());
+                chain.doFilter(requestWrapper, res);
+            } catch (Exception e) {
+                logger.error(e.toString(), e);
+                response.setStatus(HttpServletResponse.SC_BAD_GATEWAY);
+            }
+        }
     }
-  }
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see javax.servlet.Filter#destroy()
-   */
-  @Override
-  public void destroy() {}
-
+    /*
+     * (non-Javadoc)
+     *
+     * @see javax.servlet.Filter#destroy()
+     */
+    @Override
+    public void destroy() {}
 }

d6c68de964ff5d854ca3b193e9a75241ccbf77b1
Đào Quang Hưng <hung.dao1@onemount.com>
2022-09-26 09:55:34 +0700

    Backend-Service-Multimedia: validate multipart file

---
+++ src/main/java/source/anotation/ValidFile.java
@@ -0,0 +1,18 @@
+package source.anotation;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Documented
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = {FileValidator.class})
+public @interface ValidFile {
+
+    String message() default "{message.key}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ src/main/java/source/anotation/ValidFileSize.java
@@ -0,0 +1,17 @@
+package source.anotation;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Documented
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = {FileSizeValidator.class})
+public @interface ValidFileSize {
+    String message() default "{message.key}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}

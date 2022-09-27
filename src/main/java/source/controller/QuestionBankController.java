9daef86739763e393233a1d15ff9c19109a8eadd
Đào Quang Hưng <hung.dao1@onemount.com>
2022-09-27 23:10:55 +0700

    Backend-Service-QuestionBank: Add log controller

--- src/main/java/source/controller/QuestionBankController.java
+++ src/main/java/source/controller/QuestionBankController.java
@@ -2,6 +2,7 @@ package source.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import source.annotation.LogsActivityAnnotation;
 import source.constant.RouterConstant;
 import source.dto.request.CreateQuestionRequestDto;
 import source.dto.request.GetQuestionByQuestionIdRequestDto;
@@ -15,16 +16,19 @@ public class QuestionBankController {
     @Autowired
     private QuestionBankService questionBankService;
 
+    @LogsActivityAnnotation
     @PostMapping(RouterConstant.QUESTION_CREATE)
     public BaseResponse createQuestion(@RequestBody CreateQuestionRequestDto request) throws Exception {
         return questionBankService.createQuestion(request);
     }
 
+    @LogsActivityAnnotation
     @PostMapping(RouterConstant.QUESTION_GET_BY_QUESTION_ID)
     public BaseResponse getQuestionByQuestionId(@RequestBody GetQuestionByQuestionIdRequestDto request) throws Exception {
         return questionBankService.getQuestionByQuestionId(request);
     }
 
+    @LogsActivityAnnotation
     @PostMapping(RouterConstant.QUESTION_GET_ALL)
     public BaseResponse getQuestion(@RequestBody QuestionGetAllRequestDto request) throws Exception {
         return questionBankService.getAllQuestion(request);

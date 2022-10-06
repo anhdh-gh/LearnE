a2cb582f1b4cecb9431639a0a6378589adcfef75
Đào Quang Hưng <hungdq.B18CN278@stu.ptit.edu.vn>
2022-10-07 01:19:50 +0700

    Backend-Service-Course: rename enpoint get course detail

--- src/main/java/source/constant/RouterConstant.java
+++ src/main/java/source/constant/RouterConstant.java
@@ -7,12 +7,5 @@ public class RouterConstant {
     private RouterConstant() {}
 
     public static final String COURSE_CREATE = "/courses/create";
-
-    public static final String QUESTION_GET_BY_IDS = "/question/get-by-ids";
-
-    public static final String QUESTION_GET_ALL = "/question/all";
-
-    public static final String QUESTION_GET_BY_QUESTION_ID = "/question/get-by-id";
-
-    public static final String COURSE_GET_DETAILS_FOR_USER = "/courses/get-detail";
+    public static final String COURSE_GET_DETAILS_FOR_USER = "/courses/get-course-detail";
 }

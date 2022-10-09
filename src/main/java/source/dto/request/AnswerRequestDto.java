563ba17de997e9d694a3d7e02dc569e60d2352c4
Đỗ Hùng Anh <anh.do10@onemount.com>
2022-10-09 17:24:47 +0700

    Backend-Service-QuestionBank: Refactor code

--- src/main/java/source/dto/request/AnswerRequestDto.java
+++ src/main/java/source/dto/request/AnswerRequestDto.java
@@ -5,11 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Data
-@EqualsAndHashCode(callSuper = false)
 @NoArgsConstructor
 @AllArgsConstructor
+@SuperBuilder
 public class AnswerRequestDto {
 
     @JsonProperty("text")
--- src/main/java/source/dto/request/QuestionGetAllRequestDto.java
+++ src/main/java/source/dto/request/QuestionGetAllRequestDto.java
@@ -3,10 +3,12 @@ package source.dto.request;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@SuperBuilder
 public class QuestionGetAllRequestDto extends BasicRequest {
 
     private int page;

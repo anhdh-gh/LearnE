0658b7a16ed8e7e712b566cda4f096f1574cec6f
anhdh-onemount <anh.do10@onemount.com>
2022-10-16 03:03:49 +0700

    Backend-Service-User: Create HealthCheckController.java

---
+++ src/main/java/source/controller/HealthCheckController.java
@@ -0,0 +1,14 @@
+package source.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import source.dto.response.BaseResponse;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health-check")
+    public BaseResponse healthCheck() {
+        return BaseResponse.ofSucceeded("LearnE user service is running!");
+    }
+}

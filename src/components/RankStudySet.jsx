aa1804fbb32de679a7d59d0e52f4ae26763db44f
anh.do10 <anh.do10@onemount.com>
2022-10-29 00:37:18 +0700

    Frontend: Push rank component

--- src/components/RankStudySet.jsx
+++ src/components/RankStudySet.jsx
@@ -2,23 +2,28 @@ import { UserInfo, Pagination, Loader } from "."
 import { Modal } from 'react-bootstrap'
 import { useQuery } from '@tanstack/react-query'
 import { StudysetApi } from "../api"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { STATUS_CODES, ROUTE_PATH } from '../constants'
 import _ from 'lodash'
+import { RibbonContainer, RightRibbon } from 'react-ribbons'
 
 const RankStudySet = (props) => {
 
     const { studysetId, show, onHide } = props
-    const size = 10
-    const [ page, setPage ] = useState(0)
+    const size = 3
+    const [page, setPage] = useState(0)
 
-    const { data: responseGetRankStudyset, isLoading, isFetching, isError } = useQuery(
-        ["getRankStudyset"],
+    const { data: responseGetRankStudyset, isLoading, isFetching, isError, refetch: getRankStudyset } = useQuery(
+        ["getRankStudyset", page],
         () => StudysetApi.getRankStudyset(studysetId, page, size),
         {
             refetchOnWindowFocus: false,
         }
     )
+
+    useEffect(() => {
+        getRankStudyset(page)
+    }, [ page, getRankStudyset ])
 
     return <Modal show={show} fullscreen='md-down' size="lg" centered onHide={onHide}>
         <Modal.Header closeButton>
@@ -32,8 +37,11 @@ const RankStudySet = (props) => {
                     ? <Loader forComponent={true} />
                     : <Pagination className="row"
                         hrefPrev={`${ROUTE_PATH.STUDY_SET_VIEW_DETAIL}/${studysetId}`}
+                        onClickPrev={() => setPage(page - 1)}
                         hrefNext={`${ROUTE_PATH.STUDY_SET_VIEW_DETAIL}/${studysetId}`}
+                        onClickNext={() => setPage(page + 1)}
                         hrefCurrent={`${ROUTE_PATH.STUDY_SET_VIEW_DETAIL}/${studysetId}`}
+                        onClickCurrent={() => setPage(page)}
                         disabledPrev={responseGetRankStudyset?.data?.first}
                         disabledNext={responseGetRankStudyset?.data?.last}
                         page={parseInt(page) + 1}
@@ -42,8 +50,17 @@ const RankStudySet = (props) => {
                     >
                         {
                             responseGetRankStudyset?.data?.content?.map((item, index) =>
-                                <div key={item?.id}>
-                                    <UserInfo limit={50} user={item?.user} />
+                                <div key={item?.id} className="py-3 border-bottom border-1 border-primary border-start-0 border-end-0 border-top-0">
+                                    <div>
+                                        <RibbonContainer className="min-h-min">
+                                            <RightRibbon backgroundColor="#014599" color="#f0f0f0">
+                                                <span className="font-bold">{parseInt(responseGetRankStudyset?.data?.pageable?.offset) + index + 1}</span>
+                                            </RightRibbon>
+                                            <div>
+                                                <UserInfo limit={50} user={item?.user} />
+                                            </div>
+                                        </RibbonContainer>                                        
+                                    </div>
                                 </div>
                             )
                         }

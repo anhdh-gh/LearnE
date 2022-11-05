49ec7e855af13e09860f0868d2e86bfbe875446e
anh.do10 <anh.do10@onemount.com>
2022-11-06 00:37:57 +0700

    Backend-Service-Multimedia: Update storage of firebase

--- src/main/java/source/constant/FirebaseStorageConstant.java
+++ src/main/java/source/constant/FirebaseStorageConstant.java
@@ -6,7 +6,7 @@ public class FirebaseStorageConstant {
 
     public static String BASE_PATH_USER_AVATAR = "Backend-Service-Multimedia/User/Avatar/";
 
-    public static String BUCKET = "learne-bfb9c.appspot.com";
+    public static String BUCKET = "learne-41d47.appspot.com";
 
     public static String PROJECT_ID = "learne-bfb9c";
 }

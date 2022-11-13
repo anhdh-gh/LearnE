6305b85d35406a945e61245bdc90f3686ac1bc4d
anh.do10 <anh.do10@onemount.com>
2022-11-14 02:16:52 +0700

    Backend-Service-Multimedia: Update cors

--- src/main/java/source/configuration/WebConfig.java
+++ src/main/java/source/configuration/WebConfig.java
@@ -17,7 +17,6 @@ public class WebConfig implements WebMvcConfigurer {
             public void addCorsMappings(CorsRegistry registry) {
                 registry
                     .addMapping("/**")
-                    .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
                     .allowedOrigins("*");
             }
         };

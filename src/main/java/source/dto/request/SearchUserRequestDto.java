3ab69d3e63bbd28f53c8c944f0df8524cdc1f36d
anh.do10 <anh.do10@onemount.com>
2022-12-17 14:57:36 +0700

    Backend-Service-User: Update SearchUserRequestDto.java

--- src/main/java/source/dto/request/SearchUserRequestDto.java
+++ src/main/java/source/dto/request/SearchUserRequestDto.java
@@ -3,12 +3,10 @@ package source.dto.request;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-@SuperBuilder
 public class SearchUserRequestDto extends BasicRequest {
 
     private int page;

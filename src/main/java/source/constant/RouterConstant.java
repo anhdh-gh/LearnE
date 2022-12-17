8ec7b475ad0f18ddfdefb1a3102e4f27bd5cdbeb
anh.do10 <anh.do10@onemount.com>
2022-12-17 14:52:17 +0700

    Backend-Service-Gateway: Update url search user

--- src/main/java/source/constant/RouterConstant.java
+++ src/main/java/source/constant/RouterConstant.java
@@ -70,7 +70,7 @@ public class RouterConstant {
 
     public static final String COURSE_SEARCH = "/course/search";
 
-    public final static String SEARCH_USER = "user/search";
+    public final static String SEARCH_USER = "/user/search";
 
     private RouterConstant() {
     }
